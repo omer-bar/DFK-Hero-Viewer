add85158c1f0c7d8a1bec858acf3dfc26d4a8b30
EryX666 <omerbar7193@gmail.com>
2022-04-04 02:12:01 +0300

    remove files for packaging the project

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import SearchHero from "./components/SearchHero";
-
-function App() {
-	return <SearchHero />;
-}
-
-export default App;
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import fetchHero from "./utils/fetchHero";
-import HeroCard from "./Heroes/HeroCard";
-
-export { fetchHero, HeroCard };
